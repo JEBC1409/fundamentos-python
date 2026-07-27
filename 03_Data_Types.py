990640e45c4e9d86a2a1cb26e1687ccc627a7ff0
Julian <estebancuellarbermeo@gmail.com>
2026-07-27 03:17:38 -0500

    Tipos de datos y calculadora Basica

---
+++ 03_Data_Types.py
@@ -0,0 +1,8 @@
+"""Built-In Data Types"""
+
+# in programming, data types is an important concept
+# Variables can store data of different types, and different types can do different things.
+
+# Gettin Data Type
+x = 5
+print(type(x))
---
+++ Practice IA/01_Calculadora_basica.py
@@ -0,0 +1,13 @@
+num1 = float(input("ingresa el primer numero"))
+num2 = float(input("ingresa el segundo numero"))
+
+print(f"Suma: {num1+num1}")
+print(f"Resta: {num1-num2}")
+print(f"Multiplicacion {num1*num2}")
+print(f"Potenciacion: {num1**num2}")
+if num2 > 0:
+    print(f"division entera: {num1//num2}")
+    print(f"division: {num1+num2}")
+    print(f"modulo: {num1%num2}")
+else:
+    print("!No es posible dividir entre 0¡")
