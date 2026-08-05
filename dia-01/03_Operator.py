a265370ca6102c1d7106c86e33c4f0255645724f
Julian <estebancuellarbermeo@gmail.com>
2026-08-04 19:31:17 -0500

    feat: Operadores aritmeticos, comparativos y lógicos

---
+++ dia-01/03_Operator.py
@@ -0,0 +1,45 @@
+##Operadores Aritmeticos##
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(10 % 2)
+print(10 // 3)
+print(2**3)
+print(2**3 + 3 - 7 / 1 // 4)
+
+print("Hola" + "Python" + "¿Que tal?")
+print("Hola" + str(5))
+
+print("Hola" * 5)
+print("Holla" * (2**3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+##Operadores Comparativoss##
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+print(3 > 4 == 2)
+
+# No es por la cantidad de caracteres, esto es una ordenacion alfabetica por ASCII
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "Python")
+print("Hola" >= "Python")
+print("Hola" == "Python")
+print("Hola" != "Python")
+print("aaa" >= "abaa")
+print("Hola" >= "Hola")
+
+##Operadores Lógicos##
+
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or ("Hola" > "Python" and 4 == 4))
+print(not (3 > 4))
